737393e42c6abac0a09ed4f93ea11c5e37ca3eb1
Andre <andreperer4@gmail.com>
2026-02-01 15:50:14 -0300

    teste de login com sucesso e sem sucesso

---
+++ cypress/tests/e2e/login.spec.js
@@ -0,0 +1,21 @@
+describe('Real World App', () => {
+
+  it('Login com Sucesso', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="username"]').type('andrepra')
+    cy.get('[name="password"]').type('1234')
+    cy.get('[data-test="signin-submit"]').click()
+
+  })
+
+  it('Login com Falha', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="username"]').type('testecomfalha')
+    cy.get('[name="password"]').type('aaa1234')
+    cy.get('[data-test="signin-submit"]').click()
+    cy.get('[data-test="signin-error"]').should('be.visible')
+    
+  })
+   
+
+})
